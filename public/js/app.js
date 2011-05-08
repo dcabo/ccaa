58dd9c38902a833bb97fad2cb1558dfd5fbe7aa8
David Cabo <david@calibea.com>
2011-05-08 11:35:19 +0200

    first hacked ajax/json graph

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,26 @@
 $(function () {
-    var d1 = [];
-    for (var i = 0; i < 14; i += 0.5)
-        d1.push([i, Math.sin(i)]);
-
-    var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
-
-    // a null signifies separate line segments
-    var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+    var options = {
+        lines: { show: true },
+        points: { show: true },
+        xaxis: { tickDecimals: 0, tickSize: 1 }
+    };
+    var data = [];
+    var placeholder = $("#placeholder");
     
-    $.plot($("#placeholder"), [ d1, d2, d3 ]);
+    $.plot(placeholder, data, options);
+    
+    $("#fetch_button").click(function () {
+        function onDataReceived(series) {
+            data.push(series);
+            $.plot(placeholder, data, options);
+        }
+        
+        var dataurl = "/data.json";
+        $.ajax({
+            url: dataurl,
+            method: 'GET',
+            dataType: 'json',
+            success: onDataReceived
+        });
+    });
 });
